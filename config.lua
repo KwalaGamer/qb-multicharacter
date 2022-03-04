f222ab5a9201b2f04e7e8b831a493f9f8cac72dd
KwalaGamer <99956324+KwalaGamer@users.noreply.github.com>
2022-03-04 08:08:30 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.PedCoords = vector4(545.87, -2787.69, 6.1, 334.44) -- Create preview ped 
 Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(546.11, -2785.28, 6.1, 148.89) -- Camera coordinates for character preview screen
 
-Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
+--Config.DefaultNumberOfCharacters = 5 --iT Will Not Work...
 Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
     { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
 }
